e3958cbfddcabd1e9296538a8d0cabf1d804f499
duongtran93 <duongca93@gmail.com>
2019-10-09 14:42:07 +0700

    add resizeable

---
+++ Resizeable/Square.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'Resizeable.php';
+
+class Square extends Rectangle implements Resizeable
+{
+    public function __construct($width)
+    {
+        parent::__construct($width, $width);
+    }
+
+    public function resize()
+    {
+        parent::resize();
+    }
+
+    public function display()
+    {
+        return "square has: " . $this->width;
+    }
+}
